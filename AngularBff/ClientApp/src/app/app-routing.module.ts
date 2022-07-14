e83ce2675be7fa6d8c377e479957b1df69a28c6b
Scott Langley <43478262+selangley-wa@users.noreply.github.com>
2022-07-14 08:26:54 -0700

    Add bootstrap

--- AngularBff/ClientApp/src/app/app-routing.module.ts
+++ AngularBff/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { UserSessionComponent } from './user-session/user-session.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'todo', component: TodoComponent},
+  {path: 'counter', component: CounterComponent },
+  {path: 'fetch-data', component: FetchDataComponent},
+  { path: 'user-session', component: UserSessionComponent },
+  {
+    path: '',
+    redirectTo: '/todo',
+    pathMatch: 'full'
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+@NgModule({ 
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AngularBff/ClientApp/src/app/app.module.ts
+++ AngularBff/ClientApp/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { UserSessionComponent } from './user-session/user-session.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    TodoComponent,
+    UserSessionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
